49525edc553f737f0731648b32a7fabb7eb3283a
Sander Opperud Odden <sander.opperud.odden@nav.no>
2020-10-27 12:39:49 +0100

    update kotlin

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ val ktorVersion = ext.get("ktorVersion").toString()
 val mainClass = "no.nav.omsorgsdageroverforingsoknad.AppKt"
 
 plugins {
-    kotlin("jvm") version "1.3.50"
+    kotlin("jvm") version "1.4.10"
     id("com.github.johnrengelman.shadow") version "6.1.0"
 }
 
